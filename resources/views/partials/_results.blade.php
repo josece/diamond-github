3899dc670deb6be03da763f4ac9e0d717b7925df
Jose Antonio Calleja Esnal <jose@calleja.mx>
2017-11-13 15:00:59 -0600

    Feat: add follower result links for interactivity

--- resources/views/partials/_results.blade.php
+++ resources/views/partials/_results.blade.php
@@ -1,13 +1,17 @@
 @unless(empty($followers))
- <!-- Results -->
- <div class="row">
+<!-- Results -->
+<div class="row">
     <div class="col-xs-12 col-md-6">
         <h3>{{count($followers)}} {{ trans_choice('index.followers', count($followers))}} </h3>
         <ul class="followers">
-            
-        @foreach($followers as $follower)
-        <li><img class="avatar" src="{{$follower['avatar_url']}}" /> <span class="name">{{$follower['login']}}</span></li>
-        @endforeach
+            @foreach($followers as $follower)
+            <li>
+                <img class="avatar" src="{{$follower['avatar_url']}}" />  
+                <a href="{{$follower['login']}}">
+                    <span class="name">{{$follower['login']}}</span>
+                </a>
+            </li>
+            @endforeach
         </ul>
 
     </div>
